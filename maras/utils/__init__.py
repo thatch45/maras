70808a2243ebf04aa86d5b4539950b22cd96cc7d
Thomas S Hatch <thatch45@gmail.com>
2014-11-04 00:07:14 -0700

    Add rev generation via normalized timestamps

--- maras/utils/__init__.py
+++ maras/utils/__init__.py
@@ -4,7 +4,14 @@ Misc utilities
 
 # Import python libs
 import os
+import time
+import struct
 import binascii
+import datetime
+
+# create a standard epoch so all platforms will count revs from
+# a standard epoch of jan 1 2014
+STD_EPOCH = time.mktime(datetime.datetime(2014, 1, 1).timetuple())
 
 
 def rand_hex_str(size):
@@ -19,3 +26,11 @@ def rand_raw_str(size):
     Return a raw byte string of the given size
     '''
     return os.urandom(size)
+
+
+def gen_rev():
+    '''
+    Return a revision based on timestamp
+    '''
+    r_time = time.time() - STD_EPOCH
+    return struct.pack('>Q', r_time * 1000000)
