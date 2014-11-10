bef0d8848ca4ec8f69bb4410c772b3d0bae39412
Thomas S Hatch <thatch45@gmail.com>
2014-11-09 22:04:42 -0700

    initial logical mesgpack storage

--- maras/stor/mpack.py
+++ maras/stor/mpack.py
@@ -29,9 +29,29 @@ class MPack(object):
 
     def add_stor(self, fn_):
         '''
+        Open up a new storage file and add it in
         '''
+        stor_dir = os.path.dirname(fn_)
+        if not os.path.exists(stor_dir):
+            os.makedirs(stor_dir)
+        fp_ = io.open(fn_, 'r+b')
+        self.stores[fn_] = fp_
+        return fp_
 
     def insert(self, key, data, id_, ind_ref):
         '''
         '''
-        stor_data = self.get_stor(ind_ref)
+        stor = self.get_stor(ind_ref)
+        stor_str = self.data_in(data, id_)
+        stor.seek(0, 2)
+        start = stor.tell()
+        size = len(stor_str)
+        stor.write(stor_str)
+        return start, size
+
+    def data_in(self, data, id_):
+        '''
+        Serialize the data as it is sent in
+        '''
+        in_data = {'d': data, 'id_': id_}
+        return msgpack.dumps(in_data)
